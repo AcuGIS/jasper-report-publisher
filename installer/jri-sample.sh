0b71bb2fbafcea4dfc2367ea57ad396077d2ede2
Kaloyan Petrov <kaloyan_petrov@mail.bg>
2024-10-24 13:52:01 +0300

    fix samples for Jasper 7.0

--- installer/jri-sample.sh
+++ installer/jri-sample.sh
@@ -2,7 +2,7 @@
 
 source /etc/environment
 
-for ext in jasper jrxml; do
+for ext in jasper jrxml png; do
 	cp -v installer/sample/*.${ext} ${CATALINA_HOME}/jasper_reports/reports/
 	chown tomcat:tomcat ${CATALINA_HOME}/jasper_reports/reports/*.${ext}
 done

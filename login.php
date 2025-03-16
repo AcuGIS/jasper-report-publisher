073297496f785687629d1d2662e3a72ad7173a11
Kaloyan Petrov <kaloyan_petrov@mail.bg>
2025-03-16 22:28:47 +0200

    fix login title

--- login.php
+++ login.php
@@ -12,7 +12,7 @@
   <meta charset="utf-8">
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-  <title>AcuGIS | QuartzMap</title>
+  <title>AcuGIS | Jasper Report Publisher</title>
   <meta content="" name="description">
   <meta content="" name="keywords">
 
